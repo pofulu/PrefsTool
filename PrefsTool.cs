28ee549daee3f42e280f06b725751116e5f20e6a
Pofu <pofu.lu@outlook.com>
2019-01-15 20:24:05 +0800

    Add files via upload

--- PrefsTool.cs
+++ PrefsTool.cs
@@ -15,48 +15,88 @@ public class PrefsTool
             PlayerPrefs.SetString(prefsKey, onInit());
         }
     }
+    private PrefsTool(string prefsKey, Func<float> onInit)
+    {
+        this.prefsKey = prefsKey;
 
-    /// <summary>
-    /// 綁定PlayerPrefs
-    /// </summary>
-    /// <param name="prefsKey">PlayerPrefs的Key</param>
-    /// <param name="onInit">如果該Key尚未被指定過，並賦予該Key起始值；如果該Key已經有值則不會做任何事</param>
-    /// <returns></returns>
+        if (!PlayerPrefs.HasKey(prefsKey))
+        {
+            PlayerPrefs.SetFloat(prefsKey, onInit());
+        }
+    }
+    private PrefsTool(string prefsKey, Func<int> onInit)
+    {
+        this.prefsKey = prefsKey;
+
+        if (!PlayerPrefs.HasKey(prefsKey))
+        {
+            PlayerPrefs.SetInt(prefsKey, onInit());
+        }
+    }
+
+    public static void Update(string prefsKey, string value)
+    {
+        PlayerPrefs.SetString(prefsKey, value);
+    }
+    public static void Update(string prefsKey, float value)
+    {
+        PlayerPrefs.SetFloat(prefsKey, value);
+    }
+    public static void Update(string prefsKey, int value)
+    {
+        PlayerPrefs.SetInt(prefsKey, value);
+    }
+
     public static PrefsTool Bind(string prefsKey, Func<string> onInit)
     {
         return new PrefsTool(prefsKey, onInit);
     }
+    public static PrefsTool Bind(float prefsKey, Func<float> onInit)
+    {
+        return new PrefsTool(prefsKey, onInit);
+    }
+    public static PrefsTool Bind(int prefsKey, Func<int> onInit)
+    {
+        return new PrefsTool(prefsKey, onInit);
+    }
 
-    /// <summary>
-    /// 更新數值的事件
-    /// </summary>
-    /// <param name="callback"></param>
-    /// <returns></returns>
     public PrefsTool AddEvent(Action<Action<string>> callback)
     {
         callback(value => PlayerPrefs.SetString(prefsKey, value));
         return this;
     }
+    public PrefsTool AddEvent(Action<Action<float>> callback)
+    {
+        callback(value => PlayerPrefs.SetFloat(prefsKey, value));
+        return this;
+    }
+    public PrefsTool AddEvent(Action<Action<int>> callback)
+    {
+        callback(value => PlayerPrefs.SetInt(prefsKey, value));
+        return this;
+    }
 
-    /// <summary>
-    /// 更新數值的事件，例如 .AddEvnet(InputField.OnEndEdit.AddListener);
-    /// </summary>
-    /// <param name="callback"></param>
-    /// <returns></returns>
     public PrefsTool AddEvent(Action<UnityAction<string>> callback)
     {
         callback(value => PlayerPrefs.SetString(prefsKey, value));
         return this;
     }
 
-    /// <summary>
-    /// 在讀取到這個值之後觸發的事件
-    /// </summary>
-    /// <param name="callback"></param>
-    /// <returns></returns>
     public PrefsTool OnLoaded(Action<string> callback)
     {
         var value = PlayerPrefs.GetString(prefsKey);
+        callback?.Invoke(value);
+        return this;
+    }
+    public PrefsTool OnLoaded(Action<float> callback)
+    {
+        var value = PlayerPrefs.GetFloat(prefsKey);
+        callback?.Invoke(value);
+        return this;
+    }
+    public PrefsTool OnLoaded(Action<int> callback)
+    {
+        var value = PlayerPrefs.GetInt(prefsKey);
         callback?.Invoke(value);
         return this;
     }
